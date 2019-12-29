6719af97f6f00e85ac1244cc07901e903fbf114c
jomas <jomasjoao@gmail.com>
2019-12-29 00:22:08 -0300

    criação de for para diferentes sites

--- hello.go
+++ hello.go
@@ -9,6 +9,8 @@ import (
 func main() {
 
 	introducao()
+
+	//um for sem parametros, não possui critério de parada
 	for {
 		escolha := menu()
 		controllerComando(escolha)
@@ -17,13 +19,17 @@ func main() {
 }
 
 func introducao() {
-
+	//declaração de variavel normal 'var nomeDaVariavel Tipo'
+	//declaração de variavel curta ' nomeDaVariavel := Valor'
 	versao := 1.1
 	var nome string
-	fmt.Println("Qual o seu nome: ")
-	fmt.Scan(&nome)
 
+	fmt.Println("Qual o seu nome: ")
+	fmt.Println("")
+	fmt.Scan(&nome)
+	fmt.Println("")
 	fmt.Println("Olá senhor", nome+", o programa está na versão ", versao)
+	fmt.Println("")
 }
 
 func menu() int {
@@ -32,11 +38,13 @@ func menu() int {
 	fmt.Println("1 - Iniciar o monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+	fmt.Println("")
 	fmt.Scan(&escolha)
 
 	return escolha
 }
 
+//parametro da função 'Nome Tipo'
 func controllerComando(escolha int) {
 
 	switch escolha {
@@ -55,13 +63,24 @@ func controllerComando(escolha int) {
 }
 
 func iniciarMonitoramento() {
-	fmt.Println("Monitorando...")
-	resposta, _ := http.Get("https://alura.com.br")
 
-	if resposta.StatusCode == 200 {
-		fmt.Println("Carregado com sucesso")
-	} else {
-		fmt.Println("Erro ", resposta.StatusCode)
+	//slices, são arrays que não precisam declaram o tamanho
+	sites := []string{"https://alura.com.br", "https://google.com.br", "https://uol.com.br"}
+
+	//para cada 'sites' que existe, ele coloca na variavel 'item'
+	//range retorna 'posicao, variavel'
+	for _, item := range sites {
+		fmt.Println("Monitorando...", item)
+
+		//http.get retorna 'resposta, log'
+		resposta, _ := http.Get(item)
+
+		if resposta.StatusCode == 200 {
+			fmt.Println("Carregado com sucesso")
+		} else {
+			fmt.Println("Erro ", resposta.StatusCode)
+		}
+		fmt.Println("")
 	}
-
+	fmt.Println("")
 }
